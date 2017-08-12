d19f5097224a1f951c082199de8dd636ca1118d8
Bon Crowder <boncrowder@The-Bards-MacBook-Pro.local>
2017-08-11 21:26:55 -0500

    getting date with bus stop

--- Houston Run Commuter/ViewController.swift
+++ Houston Run Commuter/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
+
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("woohoo")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +24,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPush(_ sender: Any) {
 
-}
+        print("woohoo")
+        
+    
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: "https://api.ridemetro.org/data/Stops('Ho414_4620_2446')/Arrivals?&$format=json&subscription-key=8f5df090e61646659538452c75882d59")!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!.localizedDescription)
+                
+            } else {
+                
+                do {
+                    
+                    if let jsonfred = try JSONSerialization.jsonObject(with: data!) as? [String: Any]{
+                        
+                       if let d = jsonfred["d"] as? [String: Any] {
+                        
+                        if let results = d["results"] as? [[String: Any]] {
+                            for result in results {
+                                if let arrivalTime = result["LocalArrivalTime"] as? String {
+                                print(arrivalTime)
+                                }
+                            }
+                        //Implement your logic
+//                        print(results)
+                        
+                        }
+                        }
+                    }
+                    
+                } catch {
+                    
+                    print("error in JSONSerialization")
+                    
+                }
+                
+                
+            }
+            
+        
+        })
+        task.resume()
+        
+    }
+    }
 
+
+
+// https://api.ridemetro.org/data/Stops('Ho414_4620_2446')/Arrivals?&$format=json&subscription-key=8f5df090e61646659538452c75882d59
