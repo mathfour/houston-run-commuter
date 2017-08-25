74432d6f2cdfa424dfaf37cff906e3f68d5a34be
Bon Crowder <boncrowder@The-Bards-MacBook-Pro.local>
2017-08-24 21:00:18 -0500

    able to switch between entering bus stop and finding a nearby one

--- Houston Run Commuter/ViewController.swift
+++ Houston Run Commuter/ViewController.swift
@@ -40,7 +40,6 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
   @IBOutlet weak var routeNumber: UILabel!
   @IBOutlet weak var minToArrival: UILabel!
   @IBOutlet weak var textField: UITextField!
-  
   @IBOutlet weak var currentTime: UILabel!
   @IBOutlet weak var currentTimeNotDate: UILabel!
   
@@ -57,8 +56,7 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
     
     let currentLong = location.coordinate.longitude
     print(currentLong)
-    
-    
+
     manager.stopUpdatingLocation() // to stop constantly getting location
     
     currentCoordinate = manager.location?.coordinate // important
@@ -66,17 +64,120 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
   }
   
   
-  
-  
-  @IBAction func buttonPush(_ sender: Any) {
-    
+  @IBAction func buttonPushGetStopInfo(_ sender: Any) {
     let busStop : String? = self.textField.text
+    print("bus stop is \(busStop!)")
     
     self.stopNumber.text = ""
     self.minToArrival.text = ""
     self.routeNumber.text = ""
     
-    print("bus stop is \(busStop!)")
+    let config = URLSessionConfiguration.default // Session Configuration
+    let session = URLSession(configuration: config) // Load configuration into Session
+    
+    let useThisUrl: String? = "https://api.ridemetro.org/data/Stops('Ho414_4620_\(busStop!)')/Arrivals?&$format=json&subscription-key=8f5df090e61646659538452c75882d59"
+    print("*****************************")
+    print("this is the url for the routes and arrival times: " , useThisUrl!)
+    print("*****************************")
+    
+    let url = URL(string: useThisUrl!)!
+    
+    let task = session.dataTask(with: url, completionHandler: {
+      (data, response, error) in
+      
+      if error != nil {
+        
+        print(error!.localizedDescription)
+        
+      } else {
+        
+        do {
+          
+          if let jsonfred = try JSONSerialization.jsonObject(with: data!) as? [String: Any]{
+            
+            if let d = jsonfred["d"] as? [String: Any] {
+              
+              if let results = d["results"] as? [[String: Any]] {
+                
+                for result in results {
+                  
+                  if let busRoute = result["RouteName"] as? String {
+                    if let arrivalTime = result["LocalArrivalTime"] as? String {
+                      
+                      if let number = Double(arrivalTime.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                        let arTime = trunc(number/1000)
+                        print("the arTime is \(arTime)")
+                        
+                        let timezoneAdjust: Double = -5
+                        
+                        
+                        let arrival = Date(timeIntervalSince1970: TimeInterval(Double(arTime)))
+                        print("arrival time is \(arrival)")
+                        
+                        let now = trunc(Date().timeIntervalSince1970 + timezoneAdjust*60*60)
+                        
+                        print("it is now \(now)")
+                        
+                        let diff = trunc((arTime - now)/60)
+                        print("minutes until arrival is \(diff)")
+                        print("*****************************")
+                        
+                        
+                        //                                        let busStop : String? = self.textField.text
+                        //
+                        //                                        self.stopNumber.text = ""
+                        //                                        self.minToArrival.text = ""
+                        //                                        self.routeNumber.text = ""
+                        //                                                                                print("bus stop is \(busStop!)")
+                        
+                        
+                        
+                        DispatchQueue.main.async {
+                          
+                          self.stopNumber.text = self.stopNumber.text! + busStop! + "\n"
+                          
+                          self.routeNumber.text = self.routeNumber.text! + busRoute + "\n"
+                          
+                          self.minToArrival.text = self.minToArrival.text! + String(diff) + "\n"
+                        }
+                        
+                        let date = Date()
+                        let formatter = DateFormatter()
+                        formatter.dateFormat = "h:mm a"
+                        let result = formatter.string(from: date)
+                        self.currentTimeNotDate.text = result
+                        
+                      }
+                      
+                    }
+                  }
+                }
+              }
+            }
+          }
+          
+        } catch {
+          
+          print("error in JSONSerialization")
+          
+        }
+        
+        
+      }
+      
+      
+    })
+    task.resume()
+    
+  }
+
+
+
+  @IBAction func buttonPush(_ sender: Any) {
+    
+    self.stopNumber.text = ""
+    self.minToArrival.text = ""
+    self.routeNumber.text = ""
     
     let config = URLSessionConfiguration.default // Session Configuration
     let session = URLSession(configuration: config) // Load configuration into Session
